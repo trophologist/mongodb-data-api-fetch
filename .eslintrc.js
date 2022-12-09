fa1d488d9600ddbf1a9cc76caa4148b13a7ee165
andreas.sqsh <andreas.sqsh@gmail.com>
2022-12-09 17:11:23 +0100

    Clean up workspace

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['plugin:prettier/recommended'],
-  plugins: ['prettier'],
-  parser: '@typescript-eslint/parser',
-  rules: {
-    'prettier/prettier': 'error',
-    'no-console': 0
-  }
-}
--- libundler.config.ts
+++ libundler.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@surmon-china/libundler'
 
 export default defineConfig({
-  libName: 'MongoDBDataAPI',
+  libName: 'Mongo DB Data API Fetch',
   outFileName: 'mongodb-data-api-fetch',
   targets: ['cjs', 'esm'],
   entry: './src/index.ts',
